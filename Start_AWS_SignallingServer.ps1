42abb05e64d6b5e1023dba31394df84e6367239d
chloesun <chloe.cj.sun@gmail.com>
2019-02-05 11:38:38 -0500

    original setting from unreal

---
+++ Start_AWS_SignallingServer.ps1
@@ -0,0 +1,15 @@
+# Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+$PublicIp = Invoke-WebRequest -Uri "http://169.254.169.254/latest/meta-data/public-ipv4"
+
+Write-Output "Public IP: $PublicIp"
+
+$peerConnectionOptions = "{ \""iceServers\"": [{\""urls\"": [\""stun:" + $PublicIp + ":19302\""]}] }"
+
+$ProcessExe = "node.exe"
+$Arguments = @("cirrus", "--peerConnectionOptions=""$peerConnectionOptions""", "--publicIp=$PublicIp")
+# Add arguments passed to script to Arguments for executable
+$Arguments += $args
+
+Write-Output "Running: $ProcessExe $Arguments"
+Start-Process -FilePath $ProcessExe -ArgumentList $Arguments -Wait -NoNewWindow
